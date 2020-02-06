ca7ec77d2cf601fe86066e927dcfdb2abbd48110
Ozkan Yilmaz <ozkanyilmaz@gmail.com>
2020-02-06 22:31:41 +0300

    add side menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from "react";
 import AuxHoc from "../../hoc/AuxHoc";
 import styles from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
   <AuxHoc>
     <Toolbar />
+    <SideDrawer />
     <main className={styles.Content}>{props.children}</main>
   </AuxHoc>
 );
